5f91aca14d4689105080e26431f64481bd8d9537
Matteo Gottard <matteo.gottard@outlook.com>
2016-01-27 23:31:17 +0100

    Twitter auth

---
+++ server.js
@@ -0,0 +1,120 @@
+'use strict';
+
+const Hapi = require('hapi');
+const Boom = require('boom');
+
+// Create a server with a host and port
+const server = new Hapi.Server();
+server.connection({
+    port: 3000
+});
+
+// Register bell and hapi-auth-cookie with the server
+server.register([require('hapi-auth-cookie'), require('bell')], function (err) {
+
+    //Setup the session strategy
+    server.auth.strategy('session', 'cookie', {
+        password: 'secret_cookie_encryption_password', //Use something more secure in production
+        redirectTo: '/auth/twitter', //If there is no session, redirect here
+        isSecure: false //Should be set to true (which is the default) in production
+    });
+
+    //Setup the social Twitter login strategy
+    server.auth.strategy('twitter', 'bell', {
+        provider: 'twitter',
+        password: 'secret_cookie_encryption_password', //Use something more secure in production
+        clientId: 'WrGzGm0XSKHj5J3k6YHhInoyn',
+        clientSecret: 'Wl7S31Xu2nJ4D9VH3ANEnLWglk5Ljmg9BrX4VyIQIkTMgmClou',
+        isSecure: false //Should be set to true (which is the default) in production
+    });
+
+    //Setup the routes (this could be done in an own file but for the sake of simplicity isn't)
+    server.route({
+        method: 'GET',
+        path: '/auth/twitter',
+        config: {
+            auth: 'twitter', //<-- use our twitter strategy and let bell take over
+            handler: function (request, reply) {
+
+                if (!request.auth.isAuthenticated) {
+                    return reply(Boom.unauthorized('Authentication failed: ' + request.auth.error.message));
+                }
+
+                //Just store the third party credentials in the session as an example. You could do something
+                //more useful here - like loading or setting up an account (social signup).
+                request.cookieAuth.set(request.auth.credentials);
+
+                return reply.redirect('/');
+            }
+        }
+    });
+
+    //     server.route({
+    //         method: 'GET',
+    //         path: '/',
+    //         config: {
+    //             auth: 'session', //<-- require a session for this, so we have access to the twitter profile
+    //             handler: function (request, reply) {
+    // 
+    //                 //Return a message using the information from the session
+    //                 return reply('Hello, ' + request.auth.credentials.profile.displayName + '!');
+    //             }
+    //         }
+    //     });
+        
+    // This route does not require the user to be logged in.  
+    server.route({
+        method: 'GET',
+        path: '/',
+        config: {
+            handler: function homePageHandler(request, reply) {
+                return reply('Home Page');
+            }
+        }
+    });
+
+    server.route({
+        method: ['GET', 'POST'], // Must handle both GET and POST
+        path: '/login',          // The callback endpoint registered with the provider
+        config: {
+            auth: 'twitter',
+            handler: function (request, reply) {
+
+                if (!request.auth.isAuthenticated) {
+                    return reply('Authentication failed due to: ' + request.auth.error.message);
+                }
+
+                // Perform any account lookup or registration, setup local session,
+                // and redirect to the application. The third-party credentials are
+                // stored in request.auth.credentials. Any query parameters from
+                // the initial request are passed back via request.auth.credentials.query.
+                return reply.redirect('/');
+            }
+        }
+    });
+        
+    // This route is used to the logout the user.  This will **not**  
+    // logout the user from the provider they used to login.  
+    server.route({
+        method: 'GET',
+        path: '/logout',
+        config: {
+            handler: function logoutHandler(request, reply) {
+                // Clear the cookie
+                request.cookieAuth.clear();
+
+                return reply.redirect('/');
+            }
+        }
+    });
+
+    // Start the server
+    server.start((err) => {
+
+        if (err) {
+            throw err;
+        }
+
+        console.log('Server running at:', server.info.uri);
+    });
+});
